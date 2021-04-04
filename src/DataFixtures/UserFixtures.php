633f67d219354f5a9f566a893a2857e6bcf9e7b9
Wojciech Gawronski <wojciech@gawronsky.com.pl>
2021-04-04 21:07:52 +0200

    dodanie zestawu danych

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,15 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    public function __construct(private UserPasswordEncoderInterface $passwordEncoder)
+    {
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('woj@gaw');
+        $user->setUsername('woj');
+        $user->setRoles('ROLE_USER');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'password1'));
+
+        //zatwierdzenie w managerze
+        $manager->persist();
 
         $manager->flush();
     }
