77f3807a1780b089c70450a40d86f9a76236d8d8
Wojciech Gawronski <wojciech@gawronsky.com.pl>
2021-04-05 16:51:49 +0200

    przeslanie formularza do widoku

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\UploadPhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,9 @@ class IndexController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig');
+        $form = $this->createForm(UploadPhotoType::class);
+        return $this->render('index/index.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
